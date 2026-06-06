a708f1698e3628033f1d6773ecf05829ddcc0405
andrexandrex <andrejuarezcastro@gmail.com>
2026-06-05 23:41:51 -0500

    Export page object classes

---
+++ e2e_tests/pages/__init__.py
@@ -0,0 +1,11 @@
+from e2e_tests.pages.cart_page import CartPage
+from e2e_tests.pages.checkout_page import CheckoutPage
+from e2e_tests.pages.inventory_page import InventoryPage
+from e2e_tests.pages.login_page import LoginPage
+
+__all__ = [
+    "CartPage",
+    "CheckoutPage",
+    "InventoryPage",
+    "LoginPage",
+]
